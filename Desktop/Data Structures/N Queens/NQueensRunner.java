ef940d77bd0ffa910f1bad63e132a22267a4fe56
Aarthi Koripelly <akoripelly@gmail.com>
2018-12-03 23:11:14 -0600

    Remove duplicated directory

--- Desktop/Data Structures/N Queens/NQueensRunner.java
+++
@@ -1,9 +0,0 @@
-public class NQueensRunner
-{
-    public static void main(String[] args)
-    {
-        int n = (int)(Math.random() * 4 + 6);
-		NQueens queens = new NQueens(n);
-        queens.solve();
-    }
-}
